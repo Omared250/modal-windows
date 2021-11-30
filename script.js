85d37381c9efdc25a22685cb0a0b8f6b982182fb
Omar <omared250@gmail.com>
2021-11-30 14:00:51 +0100

    finish proyect

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+// close the window modal and the overlay div
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 }
+
+// Open the window modal and the overlay div
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    // Open window modal for each button
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// closing the windows modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// handle the keyboard 
+document.addEventListener('keydown', function(e) {
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
